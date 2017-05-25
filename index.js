a16b403fffd83f2679131708787defd34caaa1fa
rwblickhan <rwblickhan@gmail.com>
2017-05-24 23:55:20 -0700

    Basic database stuff... broken though

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var slackbots = require('slackbots');
 var process  = require('process');
 var path = require('path');
+var fs = require('fs');
+var sqlite = require('sqlite3');
 
 var token  = process.env.KARMABOT_API_KEY;
 var dbPath = process.env.KARMABOT_DB_PATH;
@@ -15,7 +17,17 @@ class karmabot extends slackbots {
     }
 
     connectDb() {
-        //TODO
+        if (!fs.existsSync(this.dbPath)) {
+            fs.closeSync(fs.openSync(this.dbPath, 'w'));
+        }
+        
+        this.db = new sqlite.Database(this.dbPath);
+        this.db.run('CREATE TABLE IF NOT EXISTS data (userid TEXT, points INTEGER)', function(err) {
+            if (err) {
+                console.error("Database error: " + err);
+                process.exit(1);
+            }
+        });
     }
 
     isChatMessage(data) {
@@ -36,18 +48,14 @@ class karmabot extends slackbots {
 
     updateScores(data) {
 
-        console.log(data);
-        console.log("text:" + data.text);
-
         var words = data.text.split(' ');
         var regex = /<@.+>/;
         for (var word of words) {
             if (!regex.test(word)) {
                 //not an @message
-                return;
+                continue;
             }
 
-            //TODO parse username to give points to
             var substart = word.indexOf('@');
             var subend = word.indexOf('>');
             var userid = word.substring(substart+1, subend);
@@ -73,9 +81,30 @@ class karmabot extends slackbots {
                 points -= (numNeg - 1);
             }
 
-            //TODO update database and whatnot
-            //TODO post update message
-            console.log("user " + userid + " got this many points: " + points);
+            console.log("User " + userid + " got points: " + points);
+
+            this.db.get('SELECT userid FROM data WHERE userid = ? LIMIT 1', userid, function(err, record) {
+                if (err) {
+                    return console.error("Database error: " + err);
+                }
+
+                if (!record) {
+                    this.db.run('INSERT INTO data(userid, points) VALUES(?, ?)', [userid, points], function(err) {
+                        if (err) {
+                            return console.error("Database error: " + err);
+                        }
+                        this.postMessageToChannel('general', "Hey, <@" + userid + "> now has " + points + " points!"); 
+                    });
+                    return;
+                }
+
+                this.db.run('INSERT INTO data(userid, points) VALUES(?, ?)', [userid, (record.points + points)], function(err) {
+                    if (err) {
+                        return console.error("Database error: " + err);
+                    }
+                    this.postMessageToChannel('general', "Hey, <@" + userid + "> now has " + (record.points + points) + " points!");
+                });
+            });
         }
     }
 }
@@ -89,24 +118,29 @@ var bot = new karmabot({
 bot.on('start', function() {
     //setup and such
     bot.connectDb();
-    //bot.postMessageToChannel('general', "Hey all, I'm really excited to let you know karmabot is starting up!");
+    bot.postMessageToChannel('general', "Hey all, I'm really excited to let you know karmabot is starting up!");
 });
 
 bot.on('message', function(data) {
+    console.log(data);
     //message handling and such
     if (!bot.isChatMessage(data)) {
+        console.log("Not a chat message");
         return;
     }
 
     if (!bot.isChannelConversation(data)) {
+        console.log("Not a channel conversation");
         return;
     }
 
     if (!bot.mentionsUser(data)) {
+        console.log("Doesn't mention a user");
        return;
     }
 
     if (bot.isKarmabotMessage(data)) {
+       console.log("Is a karmabot message");
        return;
     }
 
